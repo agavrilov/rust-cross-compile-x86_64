bddeb20a6de0214f3e3f68e1ebe6c07cd5cb5686
Alexander Gavrilov <agavrilov@centricsoftware.com>
2019-11-08 10:03:30 -0800

    Add Dockerfile and scripts

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build --pull -t agavrilov01/rust-cross-compile_x86_64 .
---
+++ usr/local/bin/cross-cargo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+is_mac_target ()
+{
+    for arg in "$@"
+    do
+        if [ "--target=x86_64-apple-darwin" = "$arg" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+if is_mac_target "$@"; then
+    # echo "Building for macOS"
+    # ensures that the host and target compilers are explicitly set, including using
+    # wrappers for clang for the target to pass parameters to clang that help a lot
+    # of native code more reliably compile
+    export HOST_CC=gcc
+    export HOST_CXX=g++
+    export CC=mac-cc.sh
+    export CXX=mac-c++.sh
+    export CPP="mac-cc.sh -E"
+
+    # these help resolve issues that can arise with assembly and linking
+    export AR="/usr/osxcross/bin/x86_64-apple-darwin15-ar"
+    export RANLIB="/usr/osxcross/bin/x86_64-apple-darwin15-ranlib"
+    export TOOLCHAIN_PREFIX="/usr/osxcross/bin/x86_64-apple-darwin15-"
+fi
+
+# Allow for cross-compilation of OpenSSL
+export PKG_CONFIG_ALLOW_CROSS=1
+
+exec cargo "$@"
