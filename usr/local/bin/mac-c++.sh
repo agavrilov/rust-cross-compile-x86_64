70027e7042bffb69e27232e43b0181b558cdcbf2
Alexander Gavrilov <3209180+agavrilov@users.noreply.github.com>
2021-02-26 16:25:57 -0800

    Change SDK version

--- usr/local/bin/mac-c++.sh
+++ usr/local/bin/mac-c++.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/osxcross/bin/o64-clang++ -mmacosx-version-min=10.11 -target x86_64-apple-darwin15 -mlinker-version=242 -B /usr/x86_64-apple-darwin15/bin -isysroot /usr/osxcross/SDK/MacOSX10.11.sdk $*
+exec /usr/osxcross/bin/o64-clang++ -mmacosx-version-min=10.15 -target x86_64-apple-darwin19 -B /usr/osxcross/bin -isysroot /usr/osxcross/SDK/MacOSX10.15.sdk $*
