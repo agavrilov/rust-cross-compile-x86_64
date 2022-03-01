ce01f0666e4b1a53673aeeca69f58ba829746345
Alexander Gavrilov <3209180+agavrilov@users.noreply.github.com>
2022-03-01 13:58:23 -0800

    Use Rust 1.59.0

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM agavrilov01/cross-compile-x86_64
 
-ENV RUSTUP_TOOLCHAIN=1.58.1 \
+ENV RUSTUP_TOOLCHAIN=1.59.0 \
     RUSTUP_HOME=/usr/local/rustup \
     CARGO_HOME=/usr/local/cargo \
     PATH=/usr/local/cargo/bin:$PATH
--- usr/local/bin/mac-cc.sh
+++ usr/local/bin/mac-cc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/osxcross/bin/o64-clang -mmacosx-version-min=10.15 -target x86_64-apple-darwin19 -B /usr/osxcross/bin -isysroot /usr/osxcross/SDK/MacOSX10.15.sdk $*
+exec /usr/osxcross/bin/o64-clang -mmacosx-version-min=10.15 -target x86_64-apple-darwin19 -B /usr/osxcross/bin -isysroot /usr/osxcross/SDK/MacOSX10.15.sdk "$*"
