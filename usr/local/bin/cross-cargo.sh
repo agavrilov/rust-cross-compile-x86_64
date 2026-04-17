114d55d455d58b3a1d8e7b083f1e629d92331b50
Alexander Gavrilov <3209180+agavrilov@users.noreply.github.com>
2026-04-17 12:08:47 -0700

    Add ARM targets. WIP

--- usr/local/bin/cross-cargo.sh
+++ usr/local/bin/cross-cargo.sh
@@ -4,7 +4,18 @@ is_mac_target ()
 {
     for arg in "$@"
     do
-        if [ "--target=x86_64-apple-darwin" = "$arg" ]; then
+        if [[ "$arg" =~ --target=.+-apple- ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+is_arm_target ()
+{
+    for arg in "$@"
+    do
+        if [[ "$arg" =~ --target=aarch64 || "$arg" =~ --target=arm ]]; then
             return 0
         fi
     done
@@ -12,6 +23,14 @@ is_mac_target ()
 }
 
 if is_mac_target "$@"; then
+    export SDKROOT=/usr/osxcross/SDK/MacOSX26.1.sdk
+
+    if is_arm_target "$@"; then
+        export TARGET_ARCH="aarch64"
+    else
+        export TARGET_ARCH="x86_64"
+    fi
+
     # ensures that the host and target compilers are explicitly set, including using
     # wrappers for clang for the target to pass parameters to clang that help a lot
     # of native code more reliably compile
@@ -23,9 +42,9 @@ if is_mac_target "$@"; then
 
     # these help resolve issues that can arise with assembly and linking
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/osxcross/lib"
-    export AR="/usr/osxcross/bin/x86_64-apple-darwin19-ar"
-    export RANLIB="/usr/osxcross/bin/x86_64-apple-darwin19-ranlib"
-    export TOOLCHAIN_PREFIX="/usr/osxcross/bin/x86_64-apple-darwin19-"
+    export AR="/usr/osxcross/bin/${TARGET_ARCH}-apple-darwin${DARWIN_VERSION}-ar"
+    export RANLIB="/usr/osxcross/bin/${TARGET_ARCH}-apple-darwin${DARWIN_VERSION}-ranlib"
+    export TOOLCHAIN_PREFIX="/usr/osxcross/bin/${TARGET_ARCH}-apple-darwin${DARWIN_VERSION}-"
 fi
 
 # Allow for cross-compilation of OpenSSL
